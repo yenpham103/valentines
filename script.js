b6f24ce463f95eae5b1888f1b65cb3b5fc4146e1
steph <91920601+stephanieran@users.noreply.github.com>
2025-01-20 00:01:16 -0500

    Add yes button growing

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let noClicks = 0;
+let noClicks = 1;
 const maxNoClicks = 4;
-const maxYesSize = 300;
-
+const maxYesScale = 12;
+let yesScale = 1;
 const gifElement = document.getElementById("togepi-gif")
 const noButton = document.getElementById("no-btn")
 const yesButton = document.getElementById("yes-btn")
 
 // array of gifs - in order
-// happy, sad1, sad2, crying
 const gifs = ["images/togepi-happy.gif", "images/togepi-sad-1.gif", "images/togepi-sad-2.gif", "images/togepi-crying.gif"]
 // array of messages
 const buttonMessages = ["Are you sure??", "Pookie please", "Pookie PLEASE", "You can't do this to me!"]
@@ -20,10 +19,26 @@ noButton.addEventListener("click", () => {
 
         // change no button text
         noButton.textContent = buttonMessages[noClicks]
-        noClicks++;
+
+        // Adjust button width to fit text
+        noButton.style.width = 'auto';
+        noButton.style.width = `${noButton.scrollWidth}px`;
+
+        // decrease the size of the no button
+        noButton.style.transform = `scale(${1 - noClicks * 0.1})`;
+        
     }
+    
+    if (yesScale < maxYesScale) {
+        yesScale += 2
+        // increase the size of the yes button
+        yesButton.style.transform = `scale(${yesScale})`
+    }
+
+    // increment the number of clicks
+    noClicks++;
 })
 
-yesButton.addEventListener("click", () => {
-    window.location.href = yay.html;
-})
+// yesButton.addEventListener("click", () => {
+//     window.location.href = yay.html;
+// })
