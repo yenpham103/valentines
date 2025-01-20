7271d4a18c755692c8096aac601d17e970f9277a
steph <91920601+stephanieran@users.noreply.github.com>
2025-01-20 01:51:44 -0500

    Add assets folder

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let noClicks = 1;
 const maxNoClicks = 4;
 const minNoScale = 0.65;
 let noScale = 1;
-const maxYesScale = 8;
+const maxYesScale = 6;
 let yesScale = 1;
 const gifElement = document.getElementById("togepi-gif")
 const noButton = document.getElementById("no-btn")
 const yesButton = document.getElementById("yes-btn")
 
 // array of gifs - in order
-const gifs = ["images/togepi-happy.gif", "images/togepi-sad-1.gif", "images/togepi-sad-2.gif", "images/togepi-crying.gif"]
+const gifs = ["assets/images/togepi-happy.gif", "assets/images/togepi-sad-1.gif", "assets/images/togepi-sad-2.gif", "assets/images/togepi-crying.gif"]
 // array of messages
 const buttonMessages = ["Are you sure??", "Pookie please", "Pookie PLEASE", "You can't do this to me!"]
 
@@ -35,7 +35,7 @@ noButton.addEventListener("click", () => {
     }
     
     if (yesScale < maxYesScale) {
-        yesScale += 2
+        yesScale += 1
         // increase the size of the yes button
         yesButton.style.transform = `scale(${yesScale})`
     }
